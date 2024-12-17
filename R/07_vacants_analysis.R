54bef8bd5276ac2ffa7e7a78b93361c962cefe6b
brokenspokes <121688434+brokenspokes@users.noreply.github.com>
2024-12-16 22:37:24 -0500

    final cleaning and submission

--- R/07_vacants_analysis.R
+++ R/07_vacants_analysis.R
@@ -1,10 +1,9 @@
 library(tidyverse)
 library(sf)
 library(tmap)
+library(rdeck)
 
-tmap_mode("view")
-
-st_read("neighborhood_stats.gpkg") -> neighborhood_stats
+st_read("Data/neighborhood_stats.gpkg") -> neighborhood_stats
 
 neighborhood_stats |>
   filter(vacant_n > 25) |>
@@ -25,5 +24,33 @@ neighborhood_stats |>
   mutate(diff_mean_price_ratio = regular_mean_price_ratio - vacant_mean_price_ratio,
          diff_med_price_ratio = regular_med_price_ratio - vacant_med_price_ratio) -> vacant_analysis_subset
 
-tm_shape(vacant_analysis_subset) +
-  tm_polygons(col = "diff_mean_price_ratio")
+rdeck_dark <- rdeck(map_style = mapbox_dark(),
+                    theme = "kepler",
+                    initial_bounds = st_bbox(vacant_analysis_subset),
+                    height = 600
+)
+
+vacant_analysis_subset |>
+  st_transform(4326) |>
+  st_set_geometry("polygon") -> vacant_analysis_subset
+
+rdeck_dark |>
+  add_polygon_layer(
+    name = "Comparative Assessment Value to Price ratio by Neighborhood",
+    data = vacant_analysis_subset,
+    opacity = 0.6,
+    get_fill_color = scale_color_linear(
+      diff_med_price_ratio,
+      col_label = "Difference in median price ratio"
+    ),
+    tooltip = c(Name,
+                Population,
+                regular_med_price_ratio,
+                regular_med_price,
+                vacant_med_price_ratio,
+                vacant_med_price,
+                diff_med_price_ratio),
+    pickable = TRUE
+  ) -> med_diff_assessment_value
+
+med_diff_assessment_value
